cf289ca2a450c64edfbb139d562e176ad95943b6
ochui princewill patrick <ochuiprincewill411@gmail.com>
2019-04-28 13:48:53 +0100

    modified log box

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { StyleSheet, View, Button, TextInput } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Button,
+  TextInput,
+  ScrollView,
+  Text
+} from "react-native";
 import {
   AdMobBanner,
   AdMobInterstitial,
@@ -11,75 +18,87 @@ import {
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { log: "", awardedBotton: true, interstitialBotton: true };
+    this.scroll = null;
+    this.state = { logs: [], awardedBotton: true, interstitialBotton: true };
   }
 
   componentDidMount = async () => {
     AdMobInterstitial.setTestDeviceID("EMULATOR");
     AdMobRewarded.setTestDeviceID("EMULATOR");
-    this._log("starting application\n");
+    this._log({ msg: "starting application\n", info: true });
     AdMobInterstitial.setAdUnitID("ca-app-pub-3940256099942544/1033173712"); //ca-app-pub-7832155073875042/2659325618
     AdMobRewarded.setAdUnitID("ca-app-pub-3940256099942544/1033173712"); //ca-app-pub-7832155073875042/2213916130
-    this._log("Application is Ready to Make Request\n");
+    this._log({ msg: "Application is Ready to Make Request\n", info: true });
 
     //Request ads from google admob
     try {
-      this._log("Requesting rewarded ads\n");
+      this._log({ msg: "Requesting rewarded ads\n", info: true });
       await AdMobRewarded.requestAdAsync();
-      this._log("Rewarded ads Loaded\n");
+      this._log({ msg: "Rewarded ads Loaded\n", success: true });
       this.setState({ awardedBotton: false });
     } catch (error) {
       if (error.code == "E_AD_ALREADY_LOADED") {
         this.setState({ awardedBotton: false });
+        this._log({
+          msg: `Rewarded ads request failed (${error.code})\n`,
+          warn: true
+        });
+      } else {
+        this._log({
+          msg: `Rewarded ads request failed (${error.code})\n`,
+          error: true
+        });
       }
-      this._log(`Rewarded ads request failed (${error.code})\n`);
     }
 
     try {
-      this._log("requesting Interstitial ads\n");
+      this._log({ msg: "requesting Interstitial ads\n", info: true });
       await AdMobInterstitial.requestAdAsync();
-      this._log("Interstitial ads Loaded\n");
+      this._log({ msg: "Interstitial ads Loaded\n", success: true });
       this.setState({ interstitialBotton: false });
     } catch (error) {
-      this._log(`Interstitial ads request faild (${error.code})\n`);
+      this._log({
+        msg: `Interstitial ads request faild (${error.code})\n`,
+        error: true
+      });
     }
 
     //-----Register event handlers // optional
     AdMobInterstitial.addEventListener("interstitialDidLoad", () =>
-      this._log("interstitialDidLoad \n")
+      this._log({ msg: "interstitialDidLoad \n", success: true })
     );
 
     AdMobInterstitial.addEventListener("interstitialDidFailToLoad", () =>
-      this._log("interstitialDidFailToLoad \n")
+      this._log({ msg: "interstitialDidFailToLoad \n", error: true })
     );
 
     AdMobInterstitial.addEventListener("interstitialDidOpen", () =>
-      this._log("interstitialDidOpen \n")
+      this._log({ msg: "interstitialDidOpen \n", info: true })
     );
     AdMobInterstitial.addEventListener("interstitialDidClose", () =>
-      this._log("interstitialDidClose \n")
+      this._log({ msg: "interstitialDidClose \n", info: true })
     );
     AdMobInterstitial.addEventListener("interstitialWillLeaveApplication", () =>
-      this._log("interstitialWillLeaveApplication \n")
+      this._log({ msg: "interstitialWillLeaveApplication \n", info: true })
     );
 
     AdMobRewarded.addEventListener("rewardedVideoDidRewardUser", () =>
-      this._log("rewardedVideoDidRewardUser \n")
+      this._log({ msg: "rewardedVideoDidRewardUser \n", info: true })
     );
     AdMobRewarded.addEventListener("rewardedVideoDidLoad", () =>
-      this._log("rewardedVideoDidLoad \n")
+      this._log({ msg: "rewardedVideoDidLoad \n", success: true })
     );
     AdMobRewarded.addEventListener("rewardedVideoDidFailToLoad", () =>
-      this._log("rewardedVideoDidFailToLoad \n")
+      this._log({ msg: "rewardedVideoDidFailToLoad \n", error: true })
     );
     AdMobRewarded.addEventListener("rewardedVideoDidOpen", () =>
-      this._log("rewardedVideoDidOpen \n")
+      this._log({ msg: "rewardedVideoDidOpen \n", info: true })
     );
     AdMobRewarded.addEventListener("rewardedVideoDidClose", () =>
-      this._log("rewardedVideoDidClose \n")
+      this._log({ msg: "rewardedVideoDidClose \n", info: true })
     );
     AdMobRewarded.addEventListener("rewardedVideoWillLeaveApplication", () =>
-      this._log("rewardedVideoWillLeaveApplication \n")
+      this._log({ msg: "rewardedVideoWillLeaveApplication \n", info: true })
     );
   };
 
@@ -88,7 +107,7 @@ export default class App extends React.Component {
     try {
       await AdMobInterstitial.showAdAsync();
     } catch (error) {
-      this._log(error + "\n");
+      this._log({ msg: error + "\n", error: true });
     }
   };
 
@@ -97,29 +116,33 @@ export default class App extends React.Component {
     try {
       await AdMobRewarded.showAdAsync();
     } catch (error) {
-      this._log(error + "\n");
+      this._log({ msg: error + "\n", error: true });
     }
   };
 
   bannerTwoEvent = e => {
-    this._log(`Banner Two (${e}) \n`);
+    this._log({ msg: `Banner Two (${e}) \n`, info: true });
   };
 
   bannerOneEvent = e => {
-    this._log(`Banner One (${e}) \n`);
+    this._log({ msg: `Banner One (${e}) \n`, info: true });
   };
 
   _log = log => {
-    this.setState({ log: this.state.log + log });
+    this.state.logs.push(log);
+    this.setState({
+      logs: this.state.logs
+    });
+    this.scroll.scrollToEnd();
   };
 
   bannerOneError = e => {
-    this._log(`Banner One (${e}) \n`);
+    this._log({ msg: `Banner One (${e}) \n`, error: true });
     return;
   };
 
   bannerTwoError = e => {
-    this._log(`Banner Two (${e}) \n`);
+    this._log({ msg: `Banner Two (${e}) \n`, error: true });
     return;
   };
   componentWillUnmount() {
@@ -129,6 +152,34 @@ export default class App extends React.Component {
   }
 
   render() {
+    let logArray = this.state.logs.map((log, i) => {
+      return (
+        <View
+          key={i}
+          style={{
+            height: 20,
+            borderBottomWidth: 1,
+            borderBottomColor: "#ededed"
+          }}
+        >
+          <Text
+            style={[
+              styles.logText,
+              log.error
+                ? { color: "red" }
+                : log.warn
+                ? { color: "#ffae42" }
+                : log.success
+                ? { color: "green" }
+                : null
+            ]}
+          >
+            {log.msg}
+          </Text>
+        </View>
+      );
+    });
+
     return (
       <View style={styles.container}>
         <KeepAwake />
@@ -159,14 +210,13 @@ export default class App extends React.Component {
         />
 
         <View style={styles.log}>
-          <TextInput
-            style={{ borderColor: "white", borderWidth: 1 }}
-            value={this.state.log}
-            //scrollEnabled={true}
-            editable={false}
-            multiline={true}
-            numberOfLines={4}
-          />
+          <ScrollView
+            ref={scroll => {
+              this.scroll = scroll;
+            }}
+          >
+            {logArray}
+          </ScrollView>
         </View>
 
         <PublisherBanner
@@ -214,12 +264,13 @@ const styles = StyleSheet.create({
   log: {
     top: 80,
     backgroundColor: "#fff",
-    height: 70
+    height: 150
   },
   container: {
     flex: 1,
     backgroundColor: "#000"
-    //alignItems: "center",
-    //justifyContent: "center"
+  },
+  logText: {
+    color: "blue"
   }
 });
